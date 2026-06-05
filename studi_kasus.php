da46f293b1170dc15e06214bcf2530125e062c1f
Snoop-Epin <dontaepin66@gmail.com>
2026-06-05 15:12:42 +0700

    Add files via upload

---
+++ studi_kasus.php
@@ -0,0 +1,22 @@
+<?php
+$umur = 19;
+
+if ($umur >= 18) {
+    echo "kamu sudah dewasa";
+} else {
+    echo "kamu masih anak-anak";
+}
+
+
+//studi kasus 2
+$panjang = 22;
+$lebar = 0.4;
+$luas = $panjang * $lebar;
+echo "Luas persegi panjang adalah" . $luas . "<br/>"; 
+
+
+//Latihan
+
+//buatkan code yang menghitung luas segitiga,dengan nilai alas = 11 dam tinggi = 7.
+//buatkan code program login, dengan username = admin1 dan password= xYe3ki8vp2
+//dengan ketentuan jika keduanya benar maka login berhasil
